09f6dfbcea108a5f741abba5f8e986f99e13c95e
Explore Tech With Vansh <71977043+exploretechwithvansh@users.noreply.github.com>
2022-05-03 11:09:36 +0530

    Add files via upload

---
+++ commands/core/ping.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'ping',
+    aliases: [],
+    utilisation: '{prefix}ping',
+
+    execute(client, message) {
+        const start = Date.now();
+        message.channel.send('Pong!').then(m => {
+            const embed = new Discord.MessageEmbed()
+                .setColor('BLUE')
+                .setTitle(client.user.username + " - Pong!")
+                .setThumbnail(client.user.displayAvatarURL())
+                .addField(`Message Ping`, `\`${Date.now() - start}ms\` 🛰️`)
+                .addField(`Message Latency`, `\`${m.createdTimestamp - start}ms\` 🛰️`)
+                .addField(`API Latency`, `\`${Math.round(client.ws.ping)}ms\` 🛰️`)
+                .setTimestamp()
+            m.edit({ embeds: [embed] });
+        })
+    },
+};
